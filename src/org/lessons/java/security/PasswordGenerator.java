ff5409902a579cfb56c8084e5d325ac07ed7f31e
ErikaArci99 <erikaarci1999@outlook.it>
2025-07-17 15:21:53 +0200

    Add variable declarations for user input in main method of PasswordGenerator class

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class PasswordGenerator {
 
     public static void main(String[] args) {
-
+        String name;
+        String surname;
+        String favoriteColor;
+        int birthDay;
+        int birthMonth;
+        int birthYear;
     }
 }
